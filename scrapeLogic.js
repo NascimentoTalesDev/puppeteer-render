aa55104e9a2fb76c6cc718a22063c5fd68cbc86b
NascimentoTalesDev <111396464+NascimentoTalesDev@users.noreply.github.com>
2023-09-24 11:52:21 -0300

    feat: Procfile

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,23 +1,24 @@
 const puppeteer = require('puppeteer');
 
-require("dotenv").config()
+//require("dotenv").config()
 
 
 const scrapeLogic = async (res) => {
     const browser = await puppeteer.launch({ 
-        headless: "new",
-        args: [
-            "--disable-setuid-sandbox",
-            "--no-sandbox",
-            "--single-process",
-            "--no-zygote"
-        ],
-        executablePath: process.env.NODE_ENV === 'production' 
-        ? process.env.PUPPETEER_EXECUTABLE_PATH 
-        : puppeteer.executablePath(),
+        headless: false,
+        //args: [
+        //    "--disable-setuid-sandbox",
+        //    "--no-sandbox",
+        //    "--single-process",
+        //    "--no-zygote"
+        //],
+        //executablePath: process.env.NODE_ENV === 'production' 
+        //? process.env.PUPPETEER_EXECUTABLE_PATH 
+        //: puppeteer.executablePath(),
     });
     const page = await browser.newPage();
-    let url = "https://www.magazinevoce.com.br/nascimentotales/p/dd7d4dcadh";
+    let url = "https://www.magazinevoce.com.br/nascimentotales/p/gf69ec3e2f";
+    
     // Navigate the page to a URL
     await page.goto(url)
 
@@ -26,7 +27,7 @@ const scrapeLogic = async (res) => {
         return product
        
     })
-
+    console.log(product);
     res.send(product)
 
   await browser.close();
